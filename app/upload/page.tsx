021fcd6d5afaa1ad178873ada30507ad2fc5b6b3
Fortone <81880475+Fortone@users.noreply.github.com>
2025-12-03 07:33:02 -0800

    Ensure papaparse types load correctly

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from 'react';
-import Papa from 'papaparse';
+import Papa, { ParseResult } from 'papaparse';
 import * as XLSX from 'xlsx';
 import { addInventoryItems, clearInventory } from '@/utils/db';
 
@@ -14,7 +14,7 @@ export default function UploadPage() {
       Papa.parse(file, {
         header: true,
         skipEmptyLines: true,
-        complete: (results: Papa.ParseResult<any>) => {
+        complete: (results: ParseResult<any>) => {
           resolve(results.data);
         },
         error: (err) => reject(err),
--- types/papaparse.d.ts
+++ types/papaparse.d.ts
@@ -1,39 +1,37 @@
 declare module 'papaparse' {
-  namespace Papa {
-    interface ParseError {
-      type: string;
-      code: string;
-      message: string;
-      row: number;
-    }
-
-    interface ParseMeta {
-      delimiter: string;
-      linebreak: string;
-      aborted: boolean;
-      truncated: boolean;
-      cursor: number;
-    }
-
-    interface ParseResult<T> {
-      data: T[];
-      errors: ParseError[];
-      meta: ParseMeta;
-    }
-
-    interface ParseConfig<T = any> {
-      header?: boolean;
-      skipEmptyLines?: boolean | 'greedy';
-      complete?: (results: ParseResult<T>) => void;
-      error?: (error: Error) => void;
-    }
-
-    function parse<T = any>(input: any, config?: ParseConfig<T>): ParseResult<T>;
+  export interface ParseError {
+    type: string;
+    code: string;
+    message: string;
+    row: number;
   }
 
+  export interface ParseMeta {
+    delimiter: string;
+    linebreak: string;
+    aborted: boolean;
+    truncated: boolean;
+    cursor: number;
+  }
+
+  export interface ParseResult<T> {
+    data: T[];
+    errors: ParseError[];
+    meta: ParseMeta;
+  }
+
+  export interface ParseConfig<T = any> {
+    header?: boolean;
+    skipEmptyLines?: boolean | 'greedy';
+    complete?: (results: ParseResult<T>) => void;
+    error?: (error: Error) => void;
+  }
+
+  export function parse<T = any>(input: any, config?: ParseConfig<T>): ParseResult<T>;
+
   const Papa: {
-    parse: typeof Papa.parse;
+    parse: typeof parse;
   };
 
-  export = Papa;
+  export default Papa;
 }
