eded4503868422d821337f2b671ac44db59ccbe2
Fortone <81880475+Fortone@users.noreply.github.com>
2025-12-03 07:10:52 -0800

    Fix papaparse type declarations

--- types/papaparse.d.ts
+++ types/papaparse.d.ts
@@ -1,38 +1,39 @@
-declare namespace Papa {
-  interface ParseError {
-    type: string;
-    code: string;
-    message: string;
-    row: number;
+declare module 'papaparse' {
+  namespace Papa {
+    interface ParseError {
+      type: string;
+      code: string;
+      message: string;
+      row: number;
+    }
+
+    interface ParseMeta {
+      delimiter: string;
+      linebreak: string;
+      aborted: boolean;
+      truncated: boolean;
+      cursor: number;
+    }
+
+    interface ParseResult<T> {
+      data: T[];
+      errors: ParseError[];
+      meta: ParseMeta;
+    }
+
+    interface ParseConfig<T = any> {
+      header?: boolean;
+      skipEmptyLines?: boolean | 'greedy';
+      complete?: (results: ParseResult<T>) => void;
+      error?: (error: Error) => void;
+    }
+
+    function parse<T = any>(input: any, config?: ParseConfig<T>): ParseResult<T>;
   }
 
-  interface ParseMeta {
-    delimiter: string;
-    linebreak: string;
-    aborted: boolean;
-    truncated: boolean;
-    cursor: number;
-  }
+  const Papa: {
+    parse: typeof Papa.parse;
+  };
 
-  interface ParseResult<T> {
-    data: T[];
-    errors: ParseError[];
-    meta: ParseMeta;
-  }
-
-  interface ParseConfig<T = any> {
-    header?: boolean;
-    skipEmptyLines?: boolean | 'greedy';
-    complete?: (results: ParseResult<T>) => void;
-    error?: (error: Error) => void;
-  }
-
-  function parse<T = any>(input: any, config?: ParseConfig<T>): ParseResult<T>;
+  export = Papa;
 }
-
-declare const Papa: {
-  parse: typeof Papa.parse;
-};
-
-export = Papa;
-export as namespace Papa;
